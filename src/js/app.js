32c73542b88a6c5a276702f4c766101ed3bb8867
int <yuskovss@gmail.com>
2019-11-05 00:45:15 +0400

    feat: add processing keyDown keyUp mouseDown mouseUp

--- src/js/app.js
+++ src/js/app.js
@@ -10,10 +10,10 @@ const keys = [
     ['Digit5', '5', '%', '5', '%'],
     ['Digit6', '6', ':', '6', '^'],
     ['Digit7', '7', '?', '7', '&'],
-    ['Digit8', '8', '', '8', ''],
+    ['Digit8', '8', '*', '8', '*'],
     ['Digit9', '9', '(', '9', '('],
     ['Digit0', '0', ')', '0', ')'],
-    ['Minus', '-', '', '-', ''],
+    ['Minus', '-', '_', '-', '_'],
     ['Equal', '=', '+', '=', '+'],
     ['Backspace', 'Backspace', 'Backspace', 'Backspace', 'Backspace'],
   ],
@@ -22,49 +22,49 @@ const keys = [
     ['KeyQ', 'й', 'Й', 'q', 'Q'],
     ['KeyW', 'ц', 'Ц', 'w', 'W'],
     ['KeyE', 'у', 'У', 'e', 'E'],
-    ['KeyR', '', '', '', ''],
-    ['KeyT', '', '', '', ''],
-    ['KeyY', '', '', '', ''],
-    ['KeyU', '', '', '', ''],
-    ['KeyI', '', '', '', ''],
-    ['KeyO', '', '', '', ''],
-    ['KeyP', '', '', '', ''],
-    ['BracketLeft', '', '', '', ''],
-    ['BracketRight', '', '', '', ''],
+    ['KeyR', 'к', 'К', 'r', 'R'],
+    ['KeyT', 'е', 'Е', 't', 'T'],
+    ['KeyY', 'н', 'Н', 'y', 'Y'],
+    ['KeyU', 'г', 'Г', 'u', 'U'],
+    ['KeyI', 'ш', 'Ш', 'i', 'I'],
+    ['KeyO', 'щ', 'Щ', 'o', 'O'],
+    ['KeyP', 'з', 'З', 'p', 'P'],
+    ['BracketLeft', 'х', 'Х', '[', '{'],
+    ['BracketRight', 'ъ', 'Ъ', ']', '}'],
     ['Backslash', '\\', '/', '|', '\\'],
   ],
   [
     ['CapsLock', 'CapsLock', 'CapsLock', 'CapsLock', 'CapsLock'],
-    ['KeyA', '', '', '', ''],
-    ['KeyS', '', '', '', ''],
-    ['KeyD', '', '', '', ''],
-    ['KeyF', '', '', '', ''],
-    ['KeyG', '', '', '', ''],
-    ['KeyH', '', '', '', ''],
-    ['KeyJ', '', '', '', ''],
-    ['KeyK', '', '', '', ''],
-    ['KeyL', '', '', '', ''],
-    ['Semicolon', '', '', '', ''],
-    ['Quote', '', '', '', ''],
+    ['KeyA', 'ф', 'Ф', 'a', 'A'],
+    ['KeyS', 'ы', 'Ы', 's', 'S'],
+    ['KeyD', 'в', 'В', 'd', 'D'],
+    ['KeyF', 'а', 'А', 'f', 'F'],
+    ['KeyG', 'п', 'П', 'g', 'G'],
+    ['KeyH', 'р', 'Р', 'h', 'H'],
+    ['KeyJ', 'о', 'О', 'j', 'J'],
+    ['KeyK', 'л', 'Л', 'k', 'K'],
+    ['KeyL', 'д', 'Д', 'l', 'L'],
+    ['Semicolon', 'ж', 'Ж', ';', ':'],
+    ['Quote', 'э', 'Э', '\'', '"'],
     ['Enter', 'Enter', 'Enter', 'Enter', 'Enter'],
   ],
   [
     ['ShiftLeft', 'Shift', 'Shift', 'Shift', 'Shift'],
-    ['KeyZ', '', '', '', ''],
-    ['KeyX', '', '', '', ''],
-    ['KeyC', '', '', '', ''],
-    ['KeyV', '', '', '', ''],
-    ['KeyB', '', '', '', ''],
-    ['KeyN', '', '', '', ''],
-    ['KeyM', '', '', '', ''],
-    ['Comma', '', '', '', ''],
-    ['Period', '', '', '', ''],
-    ['Slash', '', '', '', ''],
+    ['KeyZ', 'я', 'Я', 'z', 'Z'],
+    ['KeyX', 'ч', 'Ч', 'x', 'X'],
+    ['KeyC', 'с', 'С', 'c', 'C'],
+    ['KeyV', 'м', 'М', 'v', 'V'],
+    ['KeyB', 'и', 'И', 'b', 'B'],
+    ['KeyN', 'т', 'Т', 'n', 'N'],
+    ['KeyM', 'ь', 'Ь', 'm', 'M'],
+    ['Comma', 'б', 'Б', ',', '<'],
+    ['Period', 'ю', 'Ю', '.', '>'],
+    ['Slash', '.', ',', '/', '?'],
     ['ShiftRight', 'Shift', 'Shift', 'Shift', 'Shift'],
   ],
   [
     ['ControlLeft', 'Control', 'Control', 'Control', 'Control'],
-    ['MetaLeft', 'Win', 'Win', '', 'Win'],
+    ['MetaLeft', 'Win', 'Win', 'Win', 'Win'],
     ['AltLeft', 'Alt', 'Alt', 'Alt', 'Alt'],
     ['Space', '', '', '', ''],
     ['AltRight', 'Alt', 'Alt', 'Alt', 'Alt'],
@@ -73,22 +73,144 @@ const keys = [
   ],
 ];
 
+let isShift = false;
+let isCapsLock = false;
+let prevKey;
+let keyboard;
+let lang = localStorage.getItem('lang') !== 'false';
+
 const blocknote = document.createElement('textarea');
 blocknote.className = 'blocknote';
 document.body.append(blocknote);
 
-const keyboard = document.createElement('div');
-keyboard.className = 'keyboard';
-document.body.append(keyboard);
+function addSymbol(keyCode) {
+  const functionalKey = ['CapsLock', 'ShiftLeft', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'AltRight', 'MetaRight', 'ControlRight'];
+  if (!functionalKey.includes(keyCode)) {
+    let symbol;
+    switch (keyCode) {
+      case 'Space': {
+        symbol = ' ';
+        break;
+      }
+      case 'Enter': {
+        symbol = '\n';
+        break;
+      }
+      case 'Tab': {
+        symbol = '\t';
+        break;
+      }
+      case 'Backspace': {
+        symbol = '';
+        blocknote.value = blocknote.value.slice(0, blocknote.value.length - 1);
+        break;
+      }
+      default: {
+        symbol = document.querySelector(`.${keyCode.toLowerCase()}`).textContent;
+      }
+    }
+    blocknote.value += symbol;
+  }
+}
 
-keys.forEach((line) => {
-  const row = document.createElement('div');
-  row.className = 'row';
-  keyboard.append(row);
-  line.forEach((item) => {
-    const key = document.createElement('div');
-    key.className = `key ${item[0].toLowerCase()}`;
-    key.insertAdjacentText('afterbegin', `${item[1]}`);
-    row.append(key);
+function keyDown(keyCode) {
+  switch (keyCode) {
+    case 'ShiftLeft':
+    case 'ShiftRight': {
+      if (prevKey === 'AltLeft') {
+        lang = !lang;
+      }
+      if (!prevKey) {
+        isShift = true;
+        keyboard.remove();
+        keyboard = drawKeyboard();
+      }
+      break;
+    }
+    case 'AltLeft': {
+      if (prevKey === 'ShiftLeft') {
+        lang = !lang;
+      }
+      break;
+    }
+    case 'CapsLock': {
+      isCapsLock = !isCapsLock;
+      keyboard.remove();
+      keyboard = drawKeyboard();
+      break;
+    }
+    default:
+  }
+  const key = document.querySelector(`.${keyCode.toLowerCase()}`);
+  if (key) {
+    key.classList.add('pressed');
+    addSymbol(keyCode);
+  }
+  prevKey = keyCode;
+  localStorage.setItem('lang', lang);
+  console.log(lang);
+}
+
+function keyUp(keyCode) {
+  if (keyCode === 'ShiftLeft' || keyCode === 'ShiftRight') {
+    isShift = false;
+    keyboard.remove();
+    keyboard = drawKeyboard();
+  }
+
+  const key = document.querySelector(`.${keyCode.toLowerCase()}`);
+  if (key && key.classList.contains('pressed')) {
+    if (keyCode !== 'CapsLock' || (keyCode === 'CapsLock' && !isCapsLock)) {
+      key.classList.remove('pressed');
+    }
+  }
+  prevKey = undefined;
+}
+
+
+function drawKeyboard() {
+  let indexOfArray = 1;
+  if (isShift || isCapsLock) {
+    indexOfArray = 2;
+  }
+
+  if (isShift && isCapsLock) {
+    indexOfArray = 1;
+  }
+
+  if (lang) indexOfArray += 2;
+
+  const keyBoard = document.createElement('div');
+  keyBoard.className = 'keyboard';
+  document.body.append(keyBoard);
+
+  keys.forEach((line) => {
+    const row = document.createElement('div');
+    row.className = 'row';
+    keyBoard.append(row);
+    line.forEach((item) => {
+      const key = document.createElement('div');
+      key.className = `key ${item[0].toLowerCase()}`;
+      if (item[0] === 'CapsLock' && isCapsLock) {
+        key.classList.add('pressed');
+      }
+      key.insertAdjacentText('afterbegin', `${item[indexOfArray]}`);
+      key.addEventListener('mousedown', () => { keyDown(item[0]); });
+      key.addEventListener('mouseup', () => { keyUp(item[0]); });
+      row.append(key);
+    });
   });
+  return keyBoard;
+}
+
+keyboard = drawKeyboard();
+
+document.addEventListener('keydown', (event) => {
+  keyDown(event.code);
+  event.preventDefault();
+});
+
+document.addEventListener('keyup', (event) => {
+  keyUp(event.code);
+  event.preventDefault();
 });
